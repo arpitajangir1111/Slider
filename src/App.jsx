a229cc22da60891b87a3146eebb7473ba571f43c
arpitajangir1111 <arpita8857@gmail.com>
2023-08-24 11:30:53 -0700

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Slider.css";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pack, setPackage] = useState("")
+  const [page, setPage] = useState(1)
+  const [load, setLoading] = useState("")
+
+  useEffect(() => {
+    setLoading(true)
+    axios.get('https://fakestoreapi.com/products/' + page)
+      .then((product) => {
+        console.log(product.data)
+        setPackage(product.data)
+        setLoading(false)
+      })
+
+  }, [page])
+
+  function Loader() {
+    return <h2>Loading...</h2>;
+  }
 
   return (
     <>
-     <div>
-      <img src="https://m.media-amazon.com/images/I/51tKIR7GWYL._AC_UF894,1000_QL80_.jpg" alt="" />
-      <p>With his hand spiky hair, cocky personality, and contagious horselaugh, Shinchan has come an iconic character adored by both children and grown-ups. The series portrays Shinchan's everyday life, filled with humorous capriccios, funny hassles, and sportful relations with his family and musketeers.</p>
-     </div>
-    </>
+      {load === false ? (
+        <>
+          <div className="slider">
+
+
+            <div className="left">
+              <img src={pack.image} alt="product name" />
+            </div>
+            <div className="right">
+              <h3>{pack.title}</h3>
+              <p>{pack.description}</p>
+            </div>
+          </div>
+          <div className="navigation">
+            <button disabled={page === 1 ? true : false}
+              onClick={() => setPage(page - 1)}>
+              <ArrowBackIosIcon />
+            </button>
+            <button onClick={() => setPage(page + 1)}>
+              <ArrowForwardIosIcon />
+            </button>
+          </div>
+        </>
+
+      )
+      :<Loader />
+      }
+      </>
+      
   )
 }
 
-export default App
+
+
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom/client'
 // import './index.css'
 // import Counter from './Counter'
 // import Box from './Box.jsx'
-import Todo from './Todo'
-import './todo.css'
+// import Todo from './Todo'
+// import './todo.css'
+// import MovieSearch from './MovieSearch'
+import App from './App.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Todo/>
+  <App/>
   
 )
